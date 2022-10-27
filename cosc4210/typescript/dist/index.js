6f0a13a827fd9be1fd05d32b87e6eac6330dbcea
Danny Radosevich <deradosevich@gmail.com>
2022-10-27 15:02:19 -0600

    typescript

---
+++ cosc4210/typescript/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("TypeScript says hello");
+let a = 1 + 3;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple * 4;
+function squareOf(n) {
+    return (n * n);
+}
+squareOf(2);
+//squareOf("z")
+//# sourceMappingURL=index.js.map
