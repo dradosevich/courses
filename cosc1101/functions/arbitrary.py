c5c0e3d90a65bd15fa4c3d8d25cf94f9f3548da3
Danny Radosevich <deradosevich@gmail.com>
2022-10-31 10:43:34 -0600

    functions example cosc1101

---
+++ cosc1101/functions/arbitrary.py
@@ -0,0 +1,19 @@
+#Danny Radosevich
+#COSC1101
+
+def make_pizza(*toppings):
+    print(toppings)
+
+make_pizza("pepperoni")
+make_pizza("pepperoni","extra cheese", "sausage")
+
+def build_profile(first, last, **user_info):
+
+    """Build a dictionary containing everything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+user_profile = build_profile('albert', 'einstein',
+location='princeton',
+field='physics')
+print(user_profile)
